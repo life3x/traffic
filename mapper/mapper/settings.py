08d4e70a1ff8d82b2dcba9d69539038a2f5d144e
Abhirath Dubey <abhirathdubey2804@gmail.com>
2022-04-03 15:34:42 +0530

    commit

--- mapper/mapper/settings.py
+++ mapper/mapper/settings.py
@@ -93,18 +93,18 @@ WSGI_APPLICATION = 'mapper.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'traffic_mapper',
-        'USER': 'postgres',
-        'PASSWORD': 'abhirath',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'traffic_mapper',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'abhirath',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 
 }
 
